095d780a4cd3c3ffa917ac9076b3658e33b09b63
Vivian Chiang <vivian820225@gmail.com>
2021-04-04 17:14:50 +0800

    [修改] Loading 樣式修改

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app" class="text-gray-800">
-    <Loading :active.sync="isLoading" >
+    <Loading :active.sync="isLoading" class="text-center">
+      <span class="loader">
+        <span class="loader-inner"></span>
+      </span>
+      <span class="relative top-4 text-primary-default text-2xl font-ubu font-bold">
+        -- Loading --
+      </span>
     </Loading>
     <MessageAlert />
     <router-view :key="$route.fullPath" />
